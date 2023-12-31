a88478d1c67bf1fbd7e5e6cc5fc9e0c7c26491f3
Abdul Basit <basit316316@gmail.com>
2023-12-31 23:41:07 +0530

    Made Cards work like Amazon, Set up Banner that works and learned to keep images of same size

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,4 +1,10 @@
 import React, { PureComponent } from 'react'
+import logo from '../logo.svg';
+import './Header.css'
+import img1 from '../Assets/image1.jpg'
+import img2 from '../Assets/image2.jpg'
+import img3 from '../Assets/image3.jpg'
+
 
 export default class Header extends PureComponent {
     render() {
@@ -25,6 +31,34 @@ export default class Header extends PureComponent {
                         </div>
                     </div>
                 </nav>
+                <div className='Banner'>
+                    <div id="carouselExampleIndicators" className="carousel slide mb-10">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={img1} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={img2} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={img3} className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+                </div>
             </>
         )
     }
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -2,13 +2,14 @@ import React, { PureComponent } from 'react'
 import TodoItem from "./TodoItem";
 
 
-
+{/*This Code teaches a lot about how to set up a card layout like amazon. A responsive one, one which works well on phone */}
 export default class Todos extends PureComponent {
     render() {
-        let els = [1, 2, 3];
+        let els = [1, 2, 3,4,5,6,7,8];
         return (
             <>
-            <div style = {{display: 'flex', justifyContent: 'center'}}>
+            {/*Flex to go yo new line, flexwrap to make us go to next line */}
+            <div style = {{display: 'flex', justifyContent: 'center', margin: '20px 20px 20px 20px', flexWrap: 'wrap'}}>
                 {
                     els.map((el) => ( 
                         <div style={{ display: 'inline-block'}}>
